be727d075787d3f6eafe9115e924caca12da0b5a
Yashenf <spysf2002@gmail.com>
2023-04-27 01:40:31 +0530

    init and implemented some uis

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+* Created by : @yash
+*              4/26/2023 , Wednesday
+*              06:11 PM
+* Project : book_store
+* Contact me : contact.yashen@gmail.com 
+*/
+
+  const toDashboard= ()=> {
+    window.location.href='../pages/dashboard.html';
+    console.log('hello');
+}
